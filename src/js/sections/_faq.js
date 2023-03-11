e0b83c323c8ececeb820db33e1c7800a2bf38e4a
Karnauh Max <krnhmx@gmail.com>
2023-03-11 23:02:58 +0200

    fixed faq js file

--- src/js/sections/_faq.js
+++ src/js/sections/_faq.js
@@ -1,32 +1,5 @@
-// if (document.querySelector('.faq')) {
-//   const faq = document.querySelector('.faq');
-//   const items = faq.querySelectorAll('.faq__item');
-//   faq.addEventListener('click', e => {
-//     const target = e.target;
-//     const item = target.closest('.faq__item');
-
-//     if (item) {
-//       const questionContent = item.querySelector('.faq__item-body');
-//       const contentHeight = questionContent.scrollHeight;
-
-//       items.forEach(currentItem => {
-//         if (currentItem !== item && currentItem.classList.contains('active')) {
-//           currentItem.classList.remove('active');
-//           currentItem.querySelector('.faq__item-body').style.maxHeight = null;
-//         }
-//       });
-
-//       if (item.classList.contains('active')) {
-//         item.classList.remove('active');
-//         questionContent.style.maxHeight = null;
-//       } else {
-//         item.classList.add('active');
-//         questionContent.style.maxHeight = `${contentHeight}px`;
-//       }
-//     }
-//   });
-// }
-
 import Accordeon from '../functions/accordeon';
 
-new Accordeon('.faq').renderAccordeon();
+if (document.querySelector('.faq')) {
+  new Accordeon('.faq').renderAccordeon();
+}
